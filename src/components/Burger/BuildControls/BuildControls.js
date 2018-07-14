52da187d359b1e605b92117dd1d0a0d5d3c8f178
Sarah Harrington <sarah.soberg@gmail.com>
2018-07-13 23:05:21 -0500

    add and remove ingredients working, order summary modal started

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,7 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map(ctrl => (
       <BuildControl 
         key={ctrl.label} 
@@ -19,7 +20,10 @@ const buildControls = (props) => (
         added={() => props.ingredientAdded(ctrl.type)}
         removed={() => props.ingredientRemoved(ctrl.type)}
         disabled={props.disabled[ctrl.type]}/>
-    ))};
+    ))}
+    <button 
+      className={classes.OrderButton}
+      disabled={!props.purchaseable}>ORDER NOW</button>
   </div>
 );
 
